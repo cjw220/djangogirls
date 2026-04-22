bd02ad826dc43484f4b9d1d3bd385f2f8c7333c4
cjw220 <chenjo.wang@edu.em-lyon.com>
2026-04-22 15:17:06 +0200

    Update allowed hosts

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-0l*8dzl$7$j3-vat#lg#d*a&0iudimowmstka@5c$0jjhydji%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['cjw220.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
